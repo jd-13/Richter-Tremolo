db54ade7a1fc2a836780e1db66cd9a7221efa3e3
jd-13 <jackd8989@gmail.com>
2016-09-20 20:50:03 +0100

    Update headers/license

--- RichterLookAndFeel.h
+++ RichterLookAndFeel.h
@@ -1,12 +1,25 @@
 /*
-  ==============================================================================
-
-    RichterLookAndFeel.h
-    Created: 9 Jan 2016 4:33:29pm
-    Author:  Jack Devlin
-
-  ==============================================================================
-*/
+ *  File:       CustomLookAndFeel.h
+ *
+ *  Version:    2.0.0
+ *
+ *  Created:    09/01/2016
+ *
+ *	This file is part of Richter.
+ *
+ *  Richter is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Richter is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with the Richter.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef RICHTERLOOKANDFEEL_H_INCLUDED
 #define RICHTERLOOKANDFEEL_H_INCLUDED
